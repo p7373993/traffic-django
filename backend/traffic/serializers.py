76cf34f19ce37b2f4a48f9a01e492e985044d76e
sungwoo <p7373993@gmail.com>
2025-06-18 20:30:37 +0900

    사건사고 api완성성

--- backend/traffic/serializers.py
+++ backend/traffic/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Intersection, TotalTrafficVolume, TrafficVolume
+from .models import Incident
 
 class IntersectionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -18,3 +19,34 @@ class TotalTrafficVolumeSerializer(serializers.ModelSerializer):
     class Meta:
         model = TotalTrafficVolume
         fields = ['datetime', 'total_volume', 'average_speed']
+
+class IncidentSerializer(serializers.ModelSerializer):
+    intersection_name = serializers.CharField(source="intersection.name", read_only=True)
+    latitude = serializers.FloatField(source="intersection.latitude", read_only=True)
+    longitude = serializers.FloatField(source="intersection.longitude", read_only=True)
+
+    class Meta:
+        model = Incident
+        fields = [
+            "id",
+            "incident_number",
+            "ticket_number",
+            "incident_type",
+            "incident_detail_type",
+            "location_name",
+            "district",
+            "managed_by",
+            "assigned_to",
+            "description",
+            "operator",
+            "status",
+            "registered_at",
+            "last_status_update",
+            "day",
+            "month",
+            "year",
+            "intersection",
+            "intersection_name",
+            "latitude",
+            "longitude",
+        ]
